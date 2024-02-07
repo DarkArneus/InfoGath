8c4c58e9b3f0af3a934e053ad40a600a4c857c35
Darkarneus <asola268@gmail.com>
2024-02-07 23:53:58 +0100

    Added colors

--- status_code.go
+++ status_code.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net/http"
 	"sync"
+	
+	"github.com/fatih/color"
 )
 
 func statusCode(domains []string, results chan<- string, wg *sync.WaitGroup) {
@@ -11,9 +13,19 @@ func statusCode(domains []string, results chan<- string, wg *sync.WaitGroup) {
 	for _, domain := range domains {
 		resp, err := http.Get("http://" + domain)
 		if err != nil {
-			results <- fmt.Sprintf("%s: Error - %s", domain, err.Error())
+			results <- fmt.Sprintf("%s: %s", color.RedString(domain), color.RedString("Error - "+err.Error()))
 		} else {
-			results <- fmt.Sprintf("%s: Status - %s", domain, resp.Status)
+			statusColor := color.GreenString // Por defecto, el color es verde
+			switch resp.StatusCode {
+			case 404:
+				statusColor = color.RedString
+			case 302:
+				statusColor = color.BlueString
+			case 403:
+				statusColor = color.RedString
+			}
+			
+			results <- fmt.Sprintf("%s: Status - [%s]", color.CyanString(domain), statusColor(fmt.Sprintf("%s", resp.Status)))
 			resp.Body.Close()
 		}
 	}
