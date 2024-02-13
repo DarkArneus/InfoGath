dfb5015804c57e2bc79a02b73073d2bc66afaad3
Darkarneus <asola268@gmail.com>
2024-02-13 12:49:46 +0100

    No Async. Visited map. Display output.

--- src/crawl.go
+++ src/crawl.go
@@ -11,21 +11,28 @@ import (
 )
 
 func visitAnchor(domains []string, maxDepth int, results chan<- string, wg *sync.WaitGroup){
+	defer wg.Done()
+	visited := make(map[string]bool)
 	c := colly.NewCollector(
 		colly.MaxDepth(maxDepth), 
-		colly.Async(true),
+		//colly.Async(true),
 		//colly.Debugger(&debug.LogDebugger{}),
 	)
 	stop := false
 
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
 		nextlink:= e.Request.AbsoluteURL(e.Attr("href"))
-		results <-fmt.Sprintf("Visiting: %s that comes from: %s", color.YellowString(nextlink), e.Request.URL.String())
-		e.Request.Visit(nextlink)
+		if !visited[nextlink] && nextlink != ""{
+			// Mark as visited
+			visited[nextlink] = true
+			fmt.Printf("Visiting: %s that comes from: [%s]\n", color.YellowString(nextlink), color.CyanString(e.Request.URL.String()))
+			results <- fmt.Sprintf("Visiting: %s that comes from: [%s]", color.YellowString(nextlink), color.CyanString(e.Request.URL.String()))
+			e.Request.Visit(nextlink)
+		}
 	})	
 
 	c.Limit(&colly.LimitRule{
-		Parallelism: 2,
+		//Parallelism: 2,
 		RandomDelay: 5 * time.Second,
 	})
 
@@ -38,7 +45,7 @@ func visitAnchor(domains []string, maxDepth int, results chan<- string, wg *sync
 		}
 
 	}
-	c.Wait()
+	//c.Wait()
 }
 
 func crawlDetect(domains []string, results chan<- string, wg *sync.WaitGroup){
